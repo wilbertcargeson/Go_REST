8d46c1e23602165cf0c0e1d570c4797fe40c734c
wilbertcargeson <58966149+wilbertcargeson@users.noreply.github.com>
2019-12-18 14:14:51 +0700

    Add files via upload

---
+++ hash/hash.go
@@ -0,0 +1,24 @@
+package hash
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+func HashAndSalt(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+func ComparePasswords(hashedPwd string, plainPwd []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
